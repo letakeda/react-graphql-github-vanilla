add736d665a61bef482c92212d8e7f9ec92eb056
Leandro Takeda <letakeda@gmail.com>
2019-11-30 21:47:32 +0000

    Making a few changes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
+
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const TITLE = 'React GraphQL GitHub Client';
-
-const axiosGitHubGraphQL = axios.create({
-  baseURL: 'https://api.github.com/graphql',
-  headers: {
-    Authorization: `bearer ${
-      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-      }`,
-  },
-});
-
-const GET_ORGANIZATION = `{
-  organization(login: "the-road-to-react") {
-    name
-    url
-  }
-}`
+const TITLE = 'My first react with GraphQL Application';
 
 class App extends Component {
-  state = {
-    path: 'the-road-to-learn-react/the-road-to-learn-react',
-  }
-
-  //class methods
-  componentDidMount() {
-    this.onFetchFromGitHub();
-  }
-  onChange = event => {
-    this.setState({ path: event.target.value });
-  };
-  onSubmit = event => {
-    event.preventDefault();
-  };
-  onFetchFromGitHub = () => {
-    axiosGitHubGraphQL.post('', { query: GET_ORGANIZATION }).then(result => console.log(result));
-  };
-
   render() {
-    const { path } = this.state
     return (
       <div>
-        <h1>{TITLE}</h1>
-        <form onSubmit={this.onSubmit}>
-          <label htmlFor="url">Show open issues for https://github.com/</label>
-          <input id="url" type="text" onChange={this.onChange} style={{ width: '300px' }} />
-          <button type="submit">Search</button>
-        </form>
+        <h2>{TITLE}</h2>
       </div>
-    );
+    )
   }
 }
 
